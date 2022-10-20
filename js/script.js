336047c28cea1570a5bc79368fd3267c332e33a8
Eustache97 <eubomber97@gmail.com>
2022-10-20 13:11:15 +0200

    aggiunto commenti

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 //Prendo l'elemento che mi serve del DOM
 //Creo un loop che mi genera 5 volte un numero random, me li salva in un array e me li inserisce  nell'elemento del DOM
 //creo una funzione timeout che consiste nel nascondere l'elemento del DOM dopo 30 secondi (30000 millisecondi).
-//creo un ciclo in cui chiedo a l'utente di inserire un numero x5 volte e ogni volta controlla se il valore inserito dall'utente è presente nell'array dei numeri generati.
+//creo una funzione in cui chiedo a l'utente di inserire un numero x5 volte e ogni volta controlla se il valore inserito dall'utente è presente nell'array dei numeri generati.
 //Se è presente lo inserisce in un array apposito
 //alla fine stampa
 
+//Prendo l'elemento che mi serve del DOM
 const numbersContainerElement = document.querySelector(".numbers");
+//inizializzo la variabile contenebte il numero dei numeri random che voglio generare
 const numbersOfNumbers = 5;
 const generatedNumbersArray = [];
 for(let i = 0; i < numbersOfNumbers; i++){
@@ -14,23 +16,26 @@ for(let i = 0; i < numbersOfNumbers; i++){
     numbersContainerElement.innerHTML += `&nbsp;${rndNumber}`;
 }
 console.log(generatedNumbersArray);
-
+//richiamo funzione per nascondere elemento
 setTimeout(toHideElement, 30000);
-const checkedNumbersArray = [];
 
+//richiamo funzione con interazione user
+const checkedNumbersArray = [];
 setTimeout(questionToUserAndPrintResult, 31000);
 
 
-
+//funzione per generare un numero random
 function rndNumberGenerate(min, max){
     let rndNumber = Math.floor(Math.random() * max) + min;
     return rndNumber;
 }
 
+//funzione per nascondere l'elemento del DOM
 function toHideElement(){
-    numbersContainerElement.classList.add("hidden");
+    numbersContainerElement.classList.add("hidden"); 
 }
 
+/*funzione in cui chiedo a l'utente di inserire un numero x5 volte e ogni volta controlla se il valore inserito dall'utente è presente nell'array dei numeri generati. Se è presente lo inserisce in un array apposito, rimuovo l'hidden all'elemento e alla fine stampo*/
 function questionToUserAndPrintResult(){
     const numbersToRemember = "";
     const numberschecked = "";
@@ -47,6 +52,7 @@ function questionToUserAndPrintResult(){
    numbersContainerElement.classList.remove("hidden");
 }
 
+// funzione per stampare un array
 function printArray(array, element){
     for(let i = 0; i < array.length; i++){
         element += `&nbsp;${array[i]}`;
